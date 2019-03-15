91963f5eccb040d24a95ece133d3330fa087b857
juanargoti <juanargoti2015@hotmail.com>
2019-03-14 21:57:50 -0500

    nuevos script

---
+++ scripsejemplos/script1.php
@@ -0,0 +1,17 @@
+<?
+ //  mi primer scropt em php 
+ // desarrollado por : juan argoti 
+
+ $nombre = "juan";
+ $apellido ="argoti";
+ $email = "juanargoti2015@hotmail.com";
+
+
+ 
+ 
+ echo  $nombre;
+ $apllido;
+ $email;
+ "Bienvenidos";
+ 
+?>
---
+++ scripsejemplos/script2.php
@@ -0,0 +1,19 @@
+<?php
+ //  mi primer scropt em php 
+ // desarrollado por : juan argoti 
+
+ $nombre =  "juan";
+ $apellido = "argoti";
+ $email =  "juanargoti2015@hotmail.com";
+
+    echo "<center>"html en php</center";
+ 
+ 
+    echo   "Bienvenidos :" ".$nombre." " " ".$apellido.""<br>"";
+ 
+ 
+ 
+ 
+ 
+ 
+?>
