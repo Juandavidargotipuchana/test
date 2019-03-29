8740f0c1d3d3b5860fe21e5905f08347e7d6dac4
juanargoti <juanargoti2015@hotmail.com>
2019-03-28 21:58:53 -0500

    nuevos metodos

---
+++ scripsejemplos/bucle1.php
@@ -0,0 +1,44 @@
+
+
+<table border = "1">
+
+
+
+<?php
+
+
+ECHO " <b>impresion de numero en for</b>";
+	for ($i=1;$i<=10;$i++) // i++ lo mismo que  i=i+1
+	{
+		
+	echo   "<br> numero :" .i.;	
+		
+		
+	}
+	
+	
+	echo "<br> <br>";
+	
+	
+ECHO " <b>impresion de numero en while </b>";
+
+
+$j=1;
+	while ($j<=5)
+	{
+		
+		echo "<tr><td> numero: ".$j."</td></tr>;
+		$j = j  + 1 // j++
+	}
+
+
+
+
+
+
+
+ECHO " <b>impresion de numero en for</b>";
+
+
+?>
+</table>
