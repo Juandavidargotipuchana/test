39fe80d5539f68ff155379e436858fe79e881178
juanargoti <juanargoti2015@hotmail.com>
2019-03-29 19:35:53 -0500

    serie

---
+++ scripsejemplos/bucles/bucle2.php
@@ -0,0 +1,26 @@
+<?php
+
+	echo "<table border = '1'>";
+	for($i=1;$i<=10;$i++)
+	{	
+		if ($i%2==0) {
+			echo "<tr><td bgcolor='yellow'> ".$i. "</tr></td>";
+		} else {  
+			echo "<tr><td bgcolor='green'>".$i. "</tr></td>";
+		}
+	}
+	echo "</table>";
+	
+	
+	echo "<hr>"; 
+	echo "<table border = '1'> <tr>";
+	for($i=1;$i<=10;$i++)
+	{	
+		if ($i%2==0) {
+			echo "<td bgcolor='yellow'> ".$i. "</td>";
+		} else {  
+			echo "<td bgcolor='green'>".$i. "</td>";
+		}
+	}
+	echo "</table>";
+?>  
---
+++ scripsejemplos/bucles/serie.php
@@ -0,0 +1,14 @@
+
+<?php
+
+ echo   "<input type="text" name="nombredelacaja">"
+
+ 
+ 
+     echo  "<center>usuario</center><br>"
+      echo   "<center><input type="text"</center><br><br>"
+      echo  "<center> Nueva Contraseña</center><br>"
+       echo " <center><input type="password"</center><br><br>"
+ 
+
+?>  
